ae90f880c04051659e9cab3c4889cc22b120faa6
jakub-jedrusiak <kuba23031999@gmail.com>
2023-04-26 01:40:26 +0200

    tidy deps and roxygenmd

--- R/mtscr-package.R
+++ R/mtscr-package.R
@@ -2,6 +2,9 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import rlang
+#' @importFrom glue glue
+#' @importFrom lifecycle deprecated
 #' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
