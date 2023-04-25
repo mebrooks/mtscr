aa8d3bea6d9b466ea391091727f5bb46a599a9e1
jakub-jedrusiak <kuba23031999@gmail.com>
2023-04-25 18:29:49 +0200

    filter items in mtscr_creativity

--- data-raw/mtscr_creativity.R
+++ data-raw/mtscr_creativity.R
@@ -2,6 +2,15 @@
 
 mtscr_creativity <- readr::read_csv("data-raw/study2.csv") |>
   dplyr::select(-response_nofill, -item_nofill) |>
-  dplyr::filter(any(is.na(SemDis_MEAN)))
+  dplyr::filter(
+    item %in% c(
+      "belt", "brick", "broom",
+      "bucket", "candle", "clock",
+      "comb", "knife", "lamp",
+      "pencil", "pillow",
+      "purse", "sock"
+    ),
+    any(is.na(SemDis_MEAN))
+  )
 
 usethis::use_data(mtscr_creativity, overwrite = TRUE)
