f9f5547bcfe2904eba2542e36aa8af867c28603d
jakub-jedrusiak <kuba23031999@gmail.com>
2023-05-02 20:23:02 +0200

    GUI Shiny app

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# Multidimentional Top Scoring for Creativity Research
+# Multidimensional Top Scoring for Creativity Research
 
 <!-- badges: start -->
 
 [![codecov](https://codecov.io/gh/jakub-jedrusiak/mtscr/branch/master/graph/badge.svg?token=N3UGHFK5QN)](https://codecov.io/gh/jakub-jedrusiak/mtscr)
 [![R-CMD-check](https://github.com/jakub-jedrusiak/mtscr/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/jakub-jedrusiak/mtscr/actions/workflows/R-CMD-check.yaml)
+
 <!-- badges: end -->
 
-This package includes an R adaptation of Multidimentional Top Scoring
-method presented by Forthmann, Karwowski and Beaty
+An R adaptation of Multidimentional Top Scoring method presented by
+Forthmann, Karwowski and Beaty
 ([2023](https://doi.org/10.1037/aca0000571)) adapted from public code in
 the [OSF database](https://osf.io/7rgsp/). The code was adapted to use
 the [tidyverse](https://www.tidyverse.org/) framework for greater
@@ -69,14 +70,44 @@ mtscr_score(mtscr_creativity, id, item, SemDis_MEAN)
 `mtscr_model()` to get the model object. See the functions’
 documentation for more details.
 
+### Graphical User Interface
+
+This package includes a Shiny app which can be used as a GUI. Execute
+`mtscr_app()` to run it.
+
+![](./man/figures/README-GUI-example.gif)
+
+First thing you see after running the app is
+[`datamods`](https://github.com/dreamRs/datamods) window for importing
+your data. You can use the data already loaded in your environment or
+any other option. Then you’ll see four dropdown lists used to choose
+arguments for `mtscr_model()` and `mtscr_score()` functions. Consult
+these functions’ documentation for more details (execute `?mtscr_score`
+in the console). When the parameters are chosen, click “Generate model”
+button. After a while (up to a dozen or so seconds) models’ parameters
+and are shown along with a scored dataframe.
+
+You can download your data as a .csv or an .xlsx file using buttons in
+the sidebar. You can either download the scores only (i.e. the dataframe
+you see displayed) or your whole data with `.all_max` and `.all_top2`
+columns added.
+
+For testing purposes, you may use `mtscr_creativity` dataframe. In the
+importing window change “Global Environment” to “mtscr” and our
+dataframe should appear in the upper dropdown list. Use `id` for the ID
+column, `item` for the item column and `SemDis_MEAN` for the score
+column.
+
 ## Contact
 
-Correspondence concerning the merithorical side of these solutions
-should be addressed to Boris Forthmann, Institute of Psychology,
-University of Münster, Fliednerstrasse 21, 48149 Münster, Germany.
-Email: <boris.forthmann@wwu.de>.
+Correspondence concerning the meritorical side of these solutions should
+be addressed to Boris Forthmann, Institute of Psychology, University of
+Münster, Fliednerstrasse 21, 48149 Münster, Germany. Email:
+<boris.forthmann@wwu.de>.
 
-The mainteiner of the R package is Jakub Jędrusiak and the technical
+The maintainer of the R package is Jakub Jędrusiak and the technical
 concerns should be directed to him. Well, me. Best way is to open a
 [discussion on
-GitHub](https://github.com/jakub-jedrusiak/mtscr/discussions).
+GitHub](https://github.com/jakub-jedrusiak/mtscr/discussions). Technical
+difficulties may deserve an
+[issue](https://github.com/jakub-jedrusiak/mtscr/issues).
---
+++ inst/GUI/app.R
@@ -0,0 +1,159 @@
+require("shiny")
+
+# UI ----
+ui <- fluidPage(
+  titlePanel("Multidimentional Top Scoring for Creativity Research"),
+  ## Sidebar ----
+  sidebarLayout(
+    sidebarPanel(
+      width = 3,
+      actionButton("import_window", "Import data"),
+      uiOutput("args_dropdowns"),
+      uiOutput("download_buttons")
+    ),
+    ## Main panel ----
+    mainPanel(
+      fluidRow(
+        ### Loading message ----
+        tags$style(type = "text/css", "
+           #loadmessage {
+             position: fixed;
+             top: 0px;
+             left: 0px;
+             width: 100%;
+             padding: 5px 0px 5px 0px;
+             text-align: center;
+             font-weight: bold;
+             font-size: 100%;
+             color: #ffffff;
+             background-color: #0163A2;
+             z-index: 105;
+           }"),
+        conditionalPanel(
+          condition = "$('html').hasClass('shiny-busy')",
+          tags$div("Loading...", id = "loadmessage")
+        ),
+        ### Model info ----
+        uiOutput("all_max_header"),
+        tableOutput("all_max_glance"),
+        uiOutput("all_top2_header"),
+        tableOutput("all_top2_glance"),
+        uiOutput("scored_data_header"),
+        DT::dataTableOutput("scored_data")
+      )
+    )
+  )
+)
+
+# Server ----
+server <- function(input, output, session) {
+  ## Import data when run ----
+    datamods::import_modal(
+      id = "data_main",
+      from = c("env", "file", "copypaste", "googlesheets", "url"),
+      title = "Import data to be used in application"
+      )
+
+  ## Import button ----
+  observeEvent(input$import_window, {
+    datamods::import_modal(
+      id = "data_main",
+      from = c("env", "file", "copypaste", "googlesheets", "url"),
+      title = "Import data to be used in application"
+    )
+  })
+
+  imported <- datamods::import_server("data_main", return_class = "tbl_df")
+
+  ## Dropdown lists with arguments for `mtscr_model()` ----
+  output$args_dropdowns <- renderUI({
+    req(imported$data())
+    list(
+      br(),
+      selectInput("id_column", "Select ID column:", choices = colnames(imported$data())),
+      selectInput("item_column", "Select item column:", choices = colnames(imported$data())),
+      selectInput("score_column", "Select score column:", choices = colnames(imported$data())),
+      selectInput("summarise_for", "Summarise for:", choices = c("person", "item", "both")),
+      actionButton("generate_model", "Generate model →")
+    )
+  })
+
+  ## Generate model button ----
+  observeEvent(input$generate_model, {
+    ### Create model ----
+    data <- imported$data()
+    id_col <- input$id_column
+    item_col <- input$item_column
+    score_col <- input$score_column
+    model <- mtscr::mtscr_model(data, !!id_col, !!item_col, !!score_col)
+    all_max_summary <- broom.mixed::glance(model[["all_max"]])
+    all_top2_summary <- broom.mixed::glance(model[["all_top2"]])
+
+    ### Make UI for summaries ----
+    output$all_max_header <- renderUI(tags$b("All Maximum model summary:"))
+    output$all_max_glance <- renderTable(all_max_summary)
+    output$all_top2_header <- renderUI(tags$b("All Top 2 model summary:"))
+    output$all_top2_glance <- renderTable(all_top2_summary)
+
+    ### Make UI for scored data ----
+    summarise_for <- input$summarise_for
+    scored_data <- mtscr::mtscr_score(data, !!id_col, !!item_col, !!score_col, summarise_for = summarise_for)
+    scored_data_whole <- mtscr::mtscr_score(data, !!id_col, !!item_col, !!score_col, summarise_for = summarise_for, append = TRUE)
+    output$scored_data_header <- renderUI(tags$b("Scored data:"))
+    output$scored_data <- DT::renderDataTable(scored_data,
+      extensions = "Buttons",
+      options = list(
+        dom = "Bfrtip",
+        buttons = c("csv", "excel")
+      )
+    )
+
+    ### Download buttons ----
+    output$download_buttons <- renderUI(
+      list(
+        br(),
+        tags$b("Dowload scores:"),
+        br(),
+        downloadButton("scores_csv", ".csv"),
+        downloadButton("scores_xlsx", ".xlsx"),
+        br(),
+        br(),
+        tags$b("Dowload the whole database with scores:"),
+        br(),
+        downloadButton("whole_csv", ".csv"),
+        downloadButton("whole_xlsx", ".xlsx")
+      )
+    )
+    ## Download handlers ----
+    output$scores_csv <- downloadHandler(
+      filename = "scores.csv",
+      content = function(file) {
+        write.csv(scored_data, file)
+      }
+    )
+
+    output$scores_xlsx <- downloadHandler(
+      filename = "scores.xlsx",
+      content = function(file) {
+        writexl::write_xlsx(scored_data, file)
+      }
+    )
+
+    output$whole_csv <- downloadHandler(
+      filename = "whole.csv",
+      content = function(file) {
+        write.csv(scored_data_whole, file)
+      }
+    )
+
+    output$whole_xlsx <- downloadHandler(
+      filename = "whole.xlsx",
+      content = function(file) {
+        writexl::write_xlsx(scored_data_whole, file)
+      }
+    )
+  })
+}
+
+# App function ----
+shinyApp(ui, server)
